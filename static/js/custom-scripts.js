f34bd0bf4d2c2ac16a952cd97da2034a2a6f233a
Lukasz Malucha <lucasmalucha@gmail.com>
2019-03-29 15:12:41 +0000

    es6

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -83,31 +83,117 @@
 
 // OBJECT DESTRUCTURING
 
-let thingsToDo = {
-    morning: "Exercise",
-    afternoon: "Work",
-    evening: "Code",
-    night: ["Sleep", "Dream"]
-};
+//let thingsToDo = {
+//    morning: "Exercise",
+//    afternoon: "Work",
+//    evening: "Code",
+//    night: ["Sleep", "Dream"]
+//};
+//
+//let {morning, afternoon} = thingsToDo;
+//morning = 'Run'
+//
+//console.log(morning, ' - ', afternoon);
+//
+//
+//let uniStudent = ({name, university}) => {
+//    console.log(`${name} from ${university}`)
+//};
+//
+//uniStudent({
+//    name: 'Lukasz',
+//    university: 'University of Life'
+//});
+//
+//
 
-let {morning, afternoon} = thingsToDo;
-morning = 'Run'
+//// DESTRUCTURING ARRAY
+//
+//let [firstMountain] = ['Everest', 'Fish Tail', 'Annapurna'];
+//let [, secondMountain] = ['Everest', 'Fish Tail', 'Annapurna'];
+//let [, , thirdMountain] = ['Everest', 'Fish Tail', 'Annapurna'];
+//console.log(firstMountain);
+//console.log(secondMountain);
+//console.log(thirdMountain);
+//
 
-console.log(morning, ' - ', afternoon);
 
 
-let uniStudent = student => {
+// RESTRUCTURING
 
+//var adventureClimbing = {
+//    nam: 'Evererst',
+//    height: 8848,
+//    output() {
+//        console.log(`Mt. ${this.name} is ${this.height} meter tall`);
+//    }
+//};
+//
+//adventureClimbing.output();
+
+
+// SPREAD OPERATOR
+
+//var mountains = ['Everest', 'Fish Tail', 'Annapurna'];
+//var mountainsJapan = ['Fuji'];
+//
+//var allMountains = [...mountains, ...mountainsJapan];
+//console.log(allMountains);
+//
+//var day = {
+//    breakfast: 'toast with milk',
+//    lunch: 'rice with chicken curry'
+//}
+//
+//var night = {
+//    dinner: 'noodle soup'
+//}
+//
+//var picnic = {...day, ...night};
+//
+//console.log(picnic);
+//
+//
+//var letters = ['a', 'b', 'c'];
+//var[first, ..,rest] = rivers;
+//
+//console.log(rest);
+
+
+// CLASSES
+
+// parent class
+class Holiday {
+    constructor(destination, days){
+        this.destination = destination
+        this.days = days
+    }
+
+    info() {
+        console.log(`${this.destination} will take ${this.days} days.`);
+    }
 }
 
+const trip = new Holiday("Nepal" , 30);
+console.log(trip.info());
 
 
+// child class
 
+class Expedition extends holiday {
+    constructor(destination, days, gear) {
+        super(destination, days);
+        this.gear = gear;
+    }
 
+    info() {
+        super.info();
+        console.log(`Bring your ${this.gear.join(" and your")}`)
+    }
+}
 
-
-
-
+const tripWithGear = new Expedition("Everest", 30, ["Sunglasses", "Flags", "Camera"]);
+tripWithGear.info();
 
 
 
